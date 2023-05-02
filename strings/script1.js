77f50831a938b806ed68af83bc02d1692f5e0a94
awais-rafiq <124677568+awais-rafiq@users.noreply.github.com>
2023-05-02 04:03:53 -0700

    Add files via upload

---
+++ strings/script1.js
@@ -0,0 +1,159 @@
+
+// Write a ts program to find length of a string.
+// 2. Write a ts program to copy one string to another string.
+// 3. Write a ts program to concatenate two strings.
+// 4. Write a ts program to compare two strings.
+// 5. Write a ts program to convert lowercase string to uppercase.
+// 6. Write a ts program to convert uppercase string to lowercase.
+// 7. Write a ts program to toggle case of each character of a string.
+// 8. Write a ts program to find total number of alphabets, digits or special character in a string.
+// 9. Write a ts program to count total number of vowels and consonants in a string.
+// 10. Write a ts program to count total number of words in a string.
+// 11. Write a ts program to find reverse of a string.
+// 12. Write a ts program to check whether a string is palindrome or not.
+// 13. Write a ts program to reverse order of words in a given string
+// 14. Write a ts program to find first occurrence of a character in a given string.
+// 15. Write a ts program to find last occurrence of a character in a given string.
+// 16. Write a ts program to search all occurrences of a character in given string.
+// 17. Write a ts program to count occurrences of a character in given string.
+// 18. Write a ts program to find highest frequency character in a string.
+// 19. Write a ts program to find lowest frequency character in a string.
+// 20. Write a ts program to count frequency of each character in a string.
+// 21. Write a ts program to remove first occurrence of a character from string.
+// 22. Write a ts program to remove last occurrence of a character from string.
+// 23. Write a ts program to remove all occurrences of a character from string.
+// 24. Write a ts program to remove all repeated characters from a given string.
+// 25. Write a ts program to replace first occurrence of a character with another in a string.
+// 26. Write a ts program to replace last occurrence of a character with another in a string.
+// 27. Write a ts program to replace all occurrences of a character with another in a string.
+// 28. Write a ts program to find first occurrence of a word in a given string.
+// 29. Write a ts program to find last occurrence of a word in a given string.
+// 30. Write a ts program to search all occurrences of a word in given string.
+// 31. Write a ts program to count occurrences of a word in a given string.
+// 32. Write a ts program to remove first occurrence of a word from string.
+// 33. Write a ts program to remove last occurrence of a word in given string.
+// 34. Write a ts program to remove all occurrence of a word in given string.
+// 35. Write a ts program to trim leading white space characters from given string.
+// 36. Write a ts program to trim trailing white space characters from given string.
+// 37. Write a ts program to trim both leading and trailing white space characters from given string.
+// 38. Write a ts program to remove all extra blank spaces from given string.
+
+// var str = "mazhaer is happy"
+// console.log(str.length)
+
+// 2. Write a ts program to copy one string to another string.
+// let str = "awais rafiq"
+// let another= str;
+// console.log(another);
+
+
+// 3. Write a ts program to concatenate two strings.
+
+// let str = " Ameeq"
+// let anoutherStr = "Roll No 4"
+// console.log(str +" "+ anoutherStr)
+
+
+// 4. Write a ts program to compare two strings.
+
+// let str ="awais is cival enginear";
+// let str2="awais is cival enginear";
+// if(str==str2){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+
+// 5. Write a ts program to convert lowercase string to uppercase.
+
+
+//  let str ="awais is cival enginear";
+//  console.log(str.toUpperCase())
+
+
+
+// 6. Write a ts program to convert uppercase string to lowercase.
+
+// let str ="Awais is Cival Enginear";
+// console.log(str.toLowerCase())
+
+
+// 7. Write a ts program to toggle case of each character of a string.
+
+
+
+
+
+
+// 8. Write a ts program to find total number of alphabets,
+//  digits or special character in a string.
+
+
+// let str = "mazharsiddiqi3@gmail.com"
+// let strArray = Array.from(str)
+// let countCh = 0
+// let countNo = 0
+// let countSp = 0
+// for(let i = 0; i<=strArray.length;i++){
+// if(strArray[i]==)
+// }
+
+
+
+// 9. Write a ts program to count total number of vowels and consonants in a string.
+
+let str =  "awais is web developer"
+let vowels="a,e,i,o,u"
+let count = str.match()
+
+
+
+
+// 10. Write a ts program to count total number of words in a string.
+
+
+
+// 11. Write a ts program to find reverse of a string.
+
+
+
+
+// 12. Write a ts program to check whether a string is palindrome or not.
+
+
+// 13. Write a ts program to reverse order of words in a given string
+
+
+
+
+
+
+// 14. Write a ts program to find first occurrence of a character in a given string.
+
+
+
+
+
+// 15. Write a ts program to find last occurrence of a character in a given string.
+
+
+
+
+// 16. Write a ts program to search all occurrences of a character in given string.
+
+
+
+
+// 17. Write a ts program to count occurrences of a character in given string.
+
+
+
+// 18. Write a ts program to find highest frequency character in a string.
+
+
+// 19. Write a ts program to find lowest frequency character in a string.
+
+
+// 20. Write a ts program to count frequency of each character in a string.
